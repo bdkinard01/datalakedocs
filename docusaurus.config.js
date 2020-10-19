09b379418ac90d19aee36c60c6e17f31f6cb3a9d
BRANDON KINARD <brandon.kinard@kbslp.com>
2020-10-19 15:35:56 -0500

    added to gitignore

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -99,6 +99,7 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
+          baseUrl: 'docs/',
           // Please change this to your repo.
           editUrl:
             'https://github.com/facebook/docusaurus/edit/master/website/',
